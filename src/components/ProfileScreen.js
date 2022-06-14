a7566ceaa978fc8416c76d2467396b854bd07e2f
unknown <anechdz22@gmail.com>
2022-06-13 18:28:24 -0600

    first commit

---
+++ src/components/ProfileScreen.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import {Text} from 'react-native';
+
+const ProfileScreen = ({ navigation, route }) => {
+    return( 
+    <Text>El perfil del usuario es : {route.params.name}</Text>
+    )
+  }
+
+
+
+
+
+export default ProfileScreen
